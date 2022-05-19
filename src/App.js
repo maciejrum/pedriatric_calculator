35090abd7dac2a3d8e2c2e8ce9ef6f67ea94fffc
maciejrum <maciejrumian2@gmail.com>
2022-05-19 21:42:52 +0200

    adding child

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter, Routes, Route,} from "react-router-dom";
 import './App.css';
 import Login from "./components/login/Login";
 import Register from "./components/register/Register";
+import AddChild from "./components/addChild/AddChild";
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/addchild" element={<AddChild />} />
         </Routes>
       </BrowserRouter>
 
